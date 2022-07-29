d308c4bb9a9a2b168ccc845f39cc76cc101192db
LasevIX_@Laptop-lOhkM <l73yx.off@gmail.com>
2022-07-29 19:13:58 +0200

    changes

--- mplt_ex.py
+++ mplt_ex.py
@@ -1,12 +1,17 @@
 import matplotlib as mpl
+import matplotlib.backends.backend_tkagg as tkBEnd
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tkinter as tk
-data1=pd.read_csv(r"example.csv")
+data1=pd.read_csv(r"Ping_detector/example2.csv")
 dataf1=pd.DataFrame(data1)
 print(dataf1)
 root= tk.Tk()
 root.wm_title('pinging')
 fig=plt.Figure(figsize=(5,5),dpi=200)
 ax=fig.add_subplot()
+graph=tkBEnd.FigureCanvasTkAgg(fig,root)
+graph.get_tk_widget().pack()
+dataf1.plot(kind="line", legend=True, ax=ax)
+root.mainloop()
