1f7265f6a86f1d209bd4ea185daf5fdac7c4c6a6
LasevIX_@Laptop-lOhkM <l73yx.off@gmail.com>
2022-07-30 22:44:24 +0200

    figured out subprocess.run()

--- tkinter_ex.py
+++ tkinter_ex.py
@@ -7,14 +7,16 @@ import pandas as pd
 
 root=tk.Tk()
 root.wm_title("Pinging...")
+root.resizable(width=False, height=False)
 
-WelcomeLabel=tk.Label(root,text="Welcome!",font=("Arial","25"))
+WelcomeLabel=tk.Label(root,text="Welcome!",font=("Arial","25"),width=25)
 AboutButton=tk.Button(
-    root,text="About...",font=("Arial","10"),
-    command=lambda :sp.Popen(['notepad', r'README.md'])
+    root,text="About...",font=("Arial","10"),width=10,
+    command=lambda :sp.run(['notepad',r'README.md'],shell=True)
     )
+OptionsButton=tk.Button()
 
 WelcomeLabel.grid(row=0,column=0,columnspan=5)
-AboutButton.grid(row=1,column=0,columnspan=2)
+AboutButton.grid(row=1,column=0,columnspan=2,sticky="W")
 
 root.mainloop()
