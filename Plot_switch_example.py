baf8a50b73e68cedb328e4554f25d72ae3df85a1
LasevIX_@Laptop-lOhkM <l73yx.off@gmail.com>
2022-08-09 00:35:22 +0200

    figured out switching graphs

--- Plot_switch_example.py
+++ Plot_switch_example.py
@@ -8,20 +8,41 @@ import tkinter as tk
 class MainApp(tk.Tk):
     def __init__(self,**kws) -> None:
         super().__init__()
-        self.GenerateGraphs()
         self.GridMainwindow()
     
     def GridMainwindow(self):
         self.main_window_frame=tk.Frame(self)
-        self.main_window_graph=tk.Canvas(self.main_window_frame)
         self.display_graph_1=False
         self.main_window_refresh_button=tk.Button(self.main_window_frame,
             text="Refresh",command=self.RefreshGraph)
+        self.GenerateGraphs()
+        self.main_window_frame.pack()
+        self.main_window_graph.get_tk_widget().grid(row=0,column=0)
+        self.main_window_refresh_button.grid(row=0,column=1)
 
     def RefreshGraph(self):
         self.display_graph_1= not self.display_graph_1
+        if self.display_graph_1:
+            self.ax.clear()
+            self.ax.plot(self.dataf1)
+            self.main_window_graph.draw()
+        else:
+            self.ax.clear()
+            self.ax.plot(self.dataf2)
+            self.main_window_graph.draw()
     def GenerateGraphs(self):
-        data1 =pd.read_csv()
+        self.data1=pd.read_csv(r"Ping_detector/example2.csv")
+        self.data2=pd.read_csv(r"Ping_detector/example.csv")
+        self.dataf1=pd.DataFrame(self.data1)
+        self.dataf2=pd.DataFrame(self.data2)
+        self.fig=plt.Figure()
+        self.ax=self.fig.add_subplot()
+        self.ax.plot(self.dataf1)[0]
+        self.ax.set_xlabel("X AXIS")
+        self.ax.set_ylabel("Y AXIS")
+        self.main_window_graph=tkBEnd.FigureCanvasTkAgg(self.fig,master=self.main_window_frame)
+        self.main_window_graph.draw()
 
 screen=MainApp()
 screen.RefreshGraph()
+screen.mainloop()
